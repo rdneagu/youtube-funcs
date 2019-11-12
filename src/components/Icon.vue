3ef6a06ff46a646f7f037dea861645f81441d603
rdneagu <robertneagu194@gmail.com>
2019-11-12 13:37:06 +0000

    Fix icons click functionality

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,23 +1,22 @@
 <template>
-  <i class="icon" :class="[ animated, isClickable, icon ]" @click="OnClick()"></i>
+  <i class="icon" :class="[ IsClickable, icon ]" @click.stop="OnClick"></i>
 </template>
 
 <script>
 export default {
   name: 'icon',
-  props: ['animated', 'click', 'icon', 'disabled'],
+  props: ['click', 'icon', 'disabled'],
   computed: {
     // Can the icon be clicked
-    isClickable() {
-      return (this.click) ? 'clickable' : false;
+    IsClickable() {
+      return (typeof (this.click) === 'function') ? 'clickable' : false;
     },
   },
   methods: {
     // When the icon is clicked
     OnClick() {
-      if (this.disabled === false && typeof (this.click) === 'function') {
-        this.click();
-        return true;
+      if (!this.disabled && typeof (this.click) === 'function') {
+        return this.click();
       }
       return false;
     },
@@ -26,5 +25,49 @@ export default {
 </script>
 
 <style lang="scss">
-@import '@/scss/_icons.scss';
+@font-face {
+  font-family: 'ico-youtube-funcs';
+  src:
+    url('../assets/fonts/ico-youtube-funcs.ttf?plnw31') format('truetype'),
+    url('../assets/fonts/ico-youtube-funcs.woff?plnw31') format('woff'),
+    url('../assets/fonts/ico-youtube-funcs.svg?plnw31#ico-youtube-funcs') format('svg');
+  font-weight: normal;
+  font-style: normal;
+  font-display: block;
+}
+.icon {
+  font-family: 'ico-youtube-funcs' !important;
+  font-style: normal;
+  font-weight: normal;
+  font-variant: normal;
+  text-transform: none;
+  line-height: 1;
+  color: #ff2121;
+
+  /* Better Font Rendering */
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  &.clickable {
+    cursor: pointer;
+    &:hover {
+      color: lighten(#ff2121, 15%);
+    }
+  }
+}
+.icon-close:before {
+  content: "\e900";
+}
+.icon-settings:before {
+  content: "\e901";
+}
+.icon-minimize:before {
+  content: "\e902";
+}
+.icon-maximize-maxi:before {
+  content: "\e903";
+}
+.icon-maximize-mini:before {
+  content: "\e904";
+}
 </style>
