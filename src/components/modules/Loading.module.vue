20e79d91bad5fb33bc4ccb0293a77a539706de9d
rdneagu <robertneagu194@gmail.com>
2019-11-12 16:28:58 +0000

    Add relative position to .yt-logo

--- src/components/modules/Loading.module.vue
+++ src/components/modules/Loading.module.vue
@@ -2,6 +2,7 @@
   <section class="yt-loading-app">
     <div class="yt-logo">
       <img src="@/assets/images/yt_logo_mono_dark.png" />
+      
     </div>
   </section>
 </template>
@@ -19,6 +20,7 @@ export default {
   align-items: center;
   justify-content: center;
   .yt-logo {
+    position: relative;
     //background: url(../../assets/images/yt_logo_mono_dark.png) no-repeat;
   }
 }
